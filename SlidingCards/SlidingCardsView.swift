1c2e7329929201949c58c5079c0cffabcec807a8
Rashid Gaitov <rashidgaitov@gmail.com>
2022-08-03 17:08:20 +0300

    Created button, added framework protocols

---
+++ SlidingCards/SlidingCardsView.swift
@@ -0,0 +1,8 @@
+//
+//  SlidingCardsView.swift
+//  SlidingCards
+//
+//  Created by Rashid Gaitov on 03.08.2022.
+//
+
+import Foundation
--- SlidingCards/ViewController.swift
+++ SlidingCards/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import CardSlider
 
-class ViewController: UIViewController {
+struct Item: CardSliderItem {
+    var image: UIImage
+    var rating: Int?
+    var title: String
+    var subtitle: String?
+    var description: String?
+}
 
+class ViewController: UIViewController, CardSliderDataSource {
+    
+    @IBOutlet var myButton: UIButton!
+    
+    var items = [Item]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        myButton.setTitle("Show Sliding Cards", for: .normal)
+        myButton.backgroundColor = .link
+        myButton.setTitleColor(.white, for: .normal)
+        
+        myButton.layer.cornerRadius = 5
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didTapButton() {
+        
+    }
+    
+    func item(for index: Int) -> CardSliderItem {
+        <#code#>
+    }
+    
+    func numberOfItems() -> Int {
+        return 3
+    }
+    
 }
 
